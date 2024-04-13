8087e3c2597eae62710f2c527f84b520f6fdd145
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2024-04-13 18:21:41 -0300

    tests

--- chsht.rb
+++ chsht.rb
@@ -6,9 +6,8 @@ class Chsht < Formula
 
     bottle do
         root_url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/0.2.0"
-        sha256 "4921af80137af9cc3d38fd17c9120da882448a090b0a8a3a19af3199b415bfca" =>: yosemite
-        sha256 "c71db15326ee9196cd98602e38d0b7fb2b818cdd48eede4ee8eb827d809e09ba" =>: mavericks
-        sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7" =>: mountain_lion
+        sha256 cellar: "/opt/homebrew/Cellar", arm64_sonoma: "a9ae578b05c3da46cedc07dd428d94a856aeae7f3ef80a0f405bf89b8cde893a"
+        sha256 cellar: :any,                   sonoma:       "5dc376aa20241233b76e2ec2c1d4e862443a0250916b2838a1ff871e8a6dc2c5"
     end
     
     depends_on "less"
