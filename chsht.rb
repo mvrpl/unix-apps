87b7e1bd24dc534ce31218e61f544b69f0a69379
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2024-04-13 18:41:15 -0300

    adjusts conditions

--- chsht.rb
+++ chsht.rb
@@ -8,8 +8,15 @@ class Chsht < Formula
         sha256 "3e6aedea6d0c3b5da33bf869e19c1d263d98c6ded032dd6f3316e4e66300492c"
     end
 
-    if OS.mac? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-        url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/0.2.0/chsht-macos-arm64"
+    if OS.mac?
+        if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+            url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/0.2.0/chsht-macos-arm64"
+        end
+
+        if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+            url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/0.2.0/chsht-macos-amd64"
+        end
+
         sha256 "a99fa9cb2c968a283665a83af0006c1489ae8a5d775baec96dbdd1927ce4d17e"
     end
     
@@ -20,8 +27,14 @@ class Chsht < Formula
             bin.install "chsht-linux-amd64" => "chsht"
         end
 
-        if OS.mac? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-            bin.install "chsht-macos-arm64" => "chsht"
+        if OS.mac?
+            if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+                bin.install "chsht-macos-arm64" => "chsht"
+            end
+
+            if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+                bin.install "chsht-macos-amd64" => "chsht"
+            end
         end
     end
 end
