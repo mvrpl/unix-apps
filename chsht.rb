79aa4fa8c783057a36be3d150201ce67d9723f3d
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2024-04-13 18:29:59 -0300

    tests

--- chsht.rb
+++ chsht.rb
@@ -3,19 +3,14 @@ class Chsht < Formula
     homepage "https://github.com/mvrpl/Terminal-Cheat-Sheet"
     license "MIT"
 
-    bottle do
-        root_url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases"
-        sha256 cellar: "/opt/homebrew/Cellar", arm64_sonoma: "a9ae578b05c3da46cedc07dd428d94a856aeae7f3ef80a0f405bf89b8cde893a"
-        sha256 cellar: :any,                   sonoma:       "5dc376aa20241233b76e2ec2c1d4e862443a0250916b2838a1ff871e8a6dc2c5"
+    if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+        url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/0.2.0/chsht-linux-amd64"
+        sha256 "3e6aedea6d0c3b5da33bf869e19c1d263d98c6ded032dd6f3316e4e66300492c"
     end
     
     depends_on "less"
     
     def install
-        system "echo"
-    end
-    
-    test do
-        system "echo"
+        bin.install "chsht-linux-amd64" => "chsht"
     end
 end
