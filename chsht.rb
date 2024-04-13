dd47f515439115bf6d341b9d532f2761bb2581fb
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2024-04-13 18:15:14 -0300

    tests

--- chsht.rb
+++ chsht.rb
@@ -3,6 +3,13 @@ class Chsht < Formula
     homepage "https://github.com/mvrpl/Terminal-Cheat-Sheet"
     license "MIT"
     head "https://github.com/mvrpl/Terminal-Cheat-Sheet.git", branch: "main"
+
+    bottle do
+        root_url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/0.2.0"
+        sha256 "4921af80137af9cc3d38fd17c9120da882448a090b0a8a3a19af3199b415bfca" =>: yosemite
+        sha256 "c71db15326ee9196cd98602e38d0b7fb2b818cdd48eede4ee8eb827d809e09ba" =>: mavericks
+        sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7" =>: mountain_lion
+    end
     
     depends_on "less"
     
