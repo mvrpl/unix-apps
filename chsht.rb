67650f87367935df6a587c7e4e7c5a06a00c77f4
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2024-04-13 18:42:43 -0300

    adjusts

--- chsht.rb
+++ chsht.rb
@@ -2,19 +2,20 @@ class Chsht < Formula
     desc "Cheat Sheets on Terminal"
     homepage "https://github.com/mvrpl/Terminal-Cheat-Sheet"
     license "MIT"
+    version "0.2.0"
 
     if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-        url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/0.2.0/chsht-linux-amd64"
+        url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/#{version}/chsht-linux-amd64"
         sha256 "3e6aedea6d0c3b5da33bf869e19c1d263d98c6ded032dd6f3316e4e66300492c"
     end
 
     if OS.mac?
         if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-            url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/0.2.0/chsht-macos-arm64"
+            url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/#{version}/chsht-macos-arm64"
         end
 
         if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-            url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/0.2.0/chsht-macos-amd64"
+            url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/#{version}/chsht-macos-amd64"
         end
 
         sha256 "a99fa9cb2c968a283665a83af0006c1489ae8a5d775baec96dbdd1927ce4d17e"
