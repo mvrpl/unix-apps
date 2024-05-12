4397329a4edf07a9d986eb8a0f0c291175fbc7f0
Marcos Lima <mvrpl.me@gmail.com>
2024-05-12 17:31:40 -0300

    added livecheck rusync

--- rusync.rb
+++ rusync.rb
@@ -4,6 +4,12 @@ class Rusync < Formula
     license "BSD 3-Clause"
     version "0.7.2"
 
+    livecheck do
+        url :homepage
+        regex(/v?(\d+(?:\.\d+)+[a-z]?)/i)
+        strategy :github_latest
+    end
+
     if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
         url "https://github.com/your-tools/rusync/releases/download/v#{version}/rusync-linux"
         sha256 "d247e303ad932a1d34785a004e30fa692403531e02cca52ec8f76a5621718ab4"
