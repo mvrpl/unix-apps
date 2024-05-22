d306b34c5702939d61c0b7f9261a8f0a19f51fda
Marcos Lima <mvrpl.me@gmail.com>
2024-05-21 21:34:33 -0300

    adjusts formulas

--- chsht.rb
+++ chsht.rb
@@ -1,49 +1,49 @@
 class Chsht < Formula
-    desc "Cheat Sheets on Terminal"
-    homepage "https://github.com/mvrpl/Terminal-Cheat-Sheet"
-    version "0.3.0"
-    license "MIT"
+  desc "Cheat Sheets on Terminal"
+  homepage "https://github.com/mvrpl/Terminal-Cheat-Sheet"
+  version "0.3.0"
+  license "MIT"
 
-    livecheck do
-        url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/latest.json"
-        regex(/^v?(\d+(?:\.\d+)+)$/i)
-        strategy :json do |json, regex|
-            json["version"][regex, 1]
-        end
+  livecheck do
+    url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/latest.json"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :json do |json, regex|
+      json["version"][regex, 1]
+    end
+  end
+
+  if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+    url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/0.3.0/chsht-linux-amd64"
+    sha256 "c6bb7006d87b052edff370500413281db07808e929ca1fb8c2c0b9061701cc6c"
+  end
+
+  if OS.mac?
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/0.3.0/chsht-macos-arm64"
+      sha256 "0399422a638b04021fa0fd6f362d7202b2b5c9e6ad1f4c314e9db7601e9e106d"
     end
 
+    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+      url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/0.3.0/chsht-macos-amd64"
+      sha256 "64c52f54a6e2b6d0d81ad27043159f1a201c7fd1e3243b2ccabe3d7726b160f1"
+    end
+  end
+
+  depends_on "less"
+
+  def install
     if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-        url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/#{version}/chsht-linux-amd64"
-        sha256 "c6bb7006d87b052edff370500413281db07808e929ca1fb8c2c0b9061701cc6c"
+      bin.install "chsht-linux-amd64" => "chsht"
     end
 
     if OS.mac?
-        if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-            url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/#{version}/chsht-macos-arm64"
-            sha256 "0399422a638b04021fa0fd6f362d7202b2b5c9e6ad1f4c314e9db7601e9e106d"
-        end
+      if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+        bin.install "chsht-macos-arm64" => "chsht"
+      end
 
-        if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-            url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/#{version}/chsht-macos-amd64"
-            sha256 "64c52f54a6e2b6d0d81ad27043159f1a201c7fd1e3243b2ccabe3d7726b160f1"
-        end
+      if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+        bin.install "chsht-macos-amd64" => "chsht"
+      end
     end
-    
-    depends_on "less"
-    
-    def install
-        if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-            bin.install "chsht-linux-amd64" => "chsht"
-        end
-
-        if OS.mac?
-            if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-                bin.install "chsht-macos-arm64" => "chsht"
-            end
-
-            if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-                bin.install "chsht-macos-amd64" => "chsht"
-            end
-        end
-    end
+  end
 end
--- rusync.rb
+++ rusync.rb
@@ -11,12 +11,12 @@ class Rusync < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://github.com/your-tools/rusync/releases/download/v#{version}/rusync-linux"
+    url "https://github.com/your-tools/rusync/releases/download/v0.7.2/rusync-linux"
     sha256 "d247e303ad932a1d34785a004e30fa692403531e02cca52ec8f76a5621718ab4"
   end
 
   if OS.mac?
-    url "https://github.com/your-tools/rusync/releases/download/v#{version}/rusync-osx"
+    url "https://github.com/your-tools/rusync/releases/download/v0.7.2/rusync-osx"
     sha256 "7ee8ef70c26cb8bff6d967d5a01dc211c2125232deb4aa0b71d543b0479fd8e8"
   end
 
