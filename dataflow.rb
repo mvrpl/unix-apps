71456a9a06572eeeb0404df467e9ee6d98597319
Marcos Lima <mvrpl.me@gmail.com>
2024-06-01 22:05:22 -0300

    Update dataflow.rb

--- dataflow.rb
+++ dataflow.rb
@@ -19,18 +19,13 @@ class Dataflow < Formula
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/dataflow/releases/0.2.1/dataflow-aarch64-apple-darwin.tar.gz"
-      sha256 "b90422e3aa073f3a8bb8565b3a4523190ca117811a6966fc06561d3614bab35c"
+      url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/dataflow/releases/0.2.6/dataflow-aarch64-apple-darwin.tar.gz"
+      sha256 "88e957d6b630d97917486ca22b18ed37a6e18eddf68f23b4b4c9c2a1f31125c8"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/dataflow/releases/0.2.6/dataflow-x86_64-apple-darwin.tar.gz"
       sha256 "b90422e3aa073f3a8bb8565b3a4523190ca117811a6966fc06561d3614bab35c"
-    end
-
-    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/dataflow/releases/0.2.1/dataflow-x86_64-apple-darwin.tar.gz"
-      sha256 "failed"
     end
   end
 
