64ca1ebf82aa82f5f23445801f0268821d157411
mvrpl <mvrpl@users.noreply.github.com>
2024-06-02 00:56:11 +0000

    Apply automatic changes

--- dataflow.rb
+++ dataflow.rb
@@ -1,7 +1,7 @@
 class Dataflow < Formula
   desc "ETL configurable written in Rust"
   homepage "https://github.com/mvrpl/dataflow"
-  version "0.2.1"
+  version "0.2.6"
   license "GPL-3.0"
 
   livecheck do
@@ -13,19 +13,19 @@ class Dataflow < Formula
   if OS.linux? 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/dataflow/releases/0.2.1/dataflow-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "21b0aa8e874cea49036d1440053e8fde15ed2533bcbb48b75d9b24180eb0fd66"
+      sha256 "b90422e3aa073f3a8bb8565b3a4523190ca117811a6966fc06561d3614bab35c"
     end
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/dataflow/releases/0.2.1/dataflow-aarch64-apple-darwin.tar.gz"
-      sha256 "9b856b136b9e7041a5c50c89187cf7f4809550b79f47be37fb7f8f87a8eaa12a"
+      sha256 "b90422e3aa073f3a8bb8565b3a4523190ca117811a6966fc06561d3614bab35c"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/dataflow/releases/0.2.1/dataflow-x86_64-apple-darwin.tar.gz"
-      sha256 "394dc24f87bf4b20d7f1e3b962a87c3844c53c51215120be4ab421ddc36caaaa"
+      url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/dataflow/releases/0.2.6/dataflow-x86_64-apple-darwin.tar.gz"
+      sha256 "b90422e3aa073f3a8bb8565b3a4523190ca117811a6966fc06561d3614bab35c"
     end
   end
 
