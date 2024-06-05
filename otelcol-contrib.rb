e9f313cf3b509961fac33f187955062cc58f2ddc
mvrpl <mvrpl@users.noreply.github.com>
2024-06-05 23:37:57 +0000

    Apply automatic changes

--- otelcol-contrib.rb
+++ otelcol-contrib.rb
@@ -1,7 +1,7 @@
 class OtelcolContrib < Formula
   desc "OpenTelemetry Collector Contrib"
   homepage "https://github.com/open-telemetry/opentelemetry-collector-releases"
-  version "0.102.0"
+  version "0.102.1"
   license "Apache-2.0"
 
   livecheck do
@@ -11,18 +11,18 @@ class OtelcolContrib < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.102.0/otelcol-contrib_0.102.0_linux_amd64.tar.gz"
-    sha256 "e1aedaffc13f65600486ec87c50bf2e52fa748501338f73433180b801b1743a1"
+    url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.102.1/otelcol-contrib_0.102.1_linux_amd64.tar.gz"
+    sha256 "326772622016f7ff7e966a7ae8a0f439dc49a3d80b6d79a82b62608af447e851"
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.102.0/otelcol-contrib_0.102.0_darwin_arm64.tar.gz"
-      sha256 "86e69cb37e4159840c027a6b2d62418ea8e05a05d082dfaa6af823f068033680"
+      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.102.1/otelcol-contrib_0.102.1_darwin_arm64.tar.gz"
+      sha256 "f9564560798ac5c099885903f303fcda97b7ea649ec299e075b72f3805873879"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.102.0/otelcol-contrib_0.102.0_darwin_amd64.tar.gz"
-      sha256 "e3e9191075d3e184c85548512852f82381317922a229cff41250ad0917745349"
+      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.102.1/otelcol-contrib_0.102.1_darwin_amd64.tar.gz"
+      sha256 "5456734e124221e7ff775c52bd3693d05b3fac43ebe06b22aa5f220f1962ed8c"
     end
   end
 
--- speedtest.rb
+++ speedtest.rb
@@ -1,7 +1,7 @@
 class Speedtest < Formula
   desc "Runs your internet connection speed tests"
   homepage "https://github.com/mvrpl/speed-test"
-  version "1.0"
+  version "1.5"
   license "GPL-3.0"
 
   livecheck do
@@ -11,19 +11,19 @@ class Speedtest < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://github.com/mvrpl/speed-test/releases/download/1.0/speed-test_Linux_x86_64.tar.gz"
-    sha256 "a7006243ebfe2b5a5c6cf8f9f350e391fc5ceffd6028a0915cd032403de3d8c4"
+    url "https://github.com/mvrpl/speed-test/releases/download/1.5/speed-test_Linux_x86_64.tar.gz"
+    sha256 "a198aab7a460ff2beccc5c64f5e6c438191d357fb1946694a4287179cee33522"
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/mvrpl/speed-test/releases/download/1.0/speed-test_Darwin_arm64.tar.gz"
-      sha256 "94c4d876f6785e27735d3497cb73afa1f1851f111803c70eb0b2f0f754647531"
+      url "https://github.com/mvrpl/speed-test/releases/download/1.5/speed-test_Darwin_arm64.tar.gz"
+      sha256 "7639208be96bc4090eec82ae3920b1362a10cde3c73e67ee65dea2aa9b19d8ae"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/mvrpl/speed-test/releases/download/1.0/speed-test_Darwin_x86_64.tar.gz"
-      sha256 "39f39c73c7989c5f667a2c17353d85b45ca4d24545812c2f06ec1a7ef70bdd43"
+      url "https://github.com/mvrpl/speed-test/releases/download/1.5/speed-test_Darwin_x86_64.tar.gz"
+      sha256 "41e024a1030d087424ad51022e425a6fa07d63fbaafbe142638ffb1ed1d698de"
     end
   end
 
