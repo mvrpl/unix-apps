5bc513003ba302ad45033b1945f16e3bdf377f75
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2024-06-10 18:18:29 -0300

    add arm64 ccp

--- ccp.rb
+++ ccp.rb
@@ -10,7 +10,12 @@ class Ccp < Formula
     strategy :github_latest
   end
 
-  if OS.linux? 
+  if OS.linux?
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/mvrpl/ccp/releases/download/0.1.0/ccp-linux-arm64"
+      sha256 "904724c5740e4396dadeafdbe224199086443db8b78c9cead1480c6ae7b41905"
+    end
+
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/mvrpl/ccp/releases/download/0.1.0/ccp-linux-amd64"
       sha256 "9dc46c625150133d09dd6713b797b3d08e50a40a5dbc3c80749f2941be7da0c8"
@@ -30,7 +35,10 @@ class Ccp < Formula
   end
 
   def install
-    if OS.linux? 
+    if OS.linux?
+      if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+        bin.install "ccp-linux-arm64"  => "ccp"
+      end
       if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
         bin.install "ccp-linux-amd64"  => "ccp"
       end
