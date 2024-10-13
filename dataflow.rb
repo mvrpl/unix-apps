d29053a815fdb2a79aff18b5aa1dc7fa21c313d9
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2024-10-12 21:24:08 -0300

    test

--- dataflow.rb
+++ dataflow.rb
@@ -4,11 +4,18 @@ class Dataflow < Formula
   version "0.3.0"
   license "GPL-3.0"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "4fcdce1b4936433d5914e3e7143f9e429e651d1139fed1610b73bfd5c1e7ba66"
-  end
+  url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/dataflow/releases/0.3.0/dataflow-0.3.0.tar.gz"
+  sha256 "b691cf2f5dd179131a80bbab840bde268d075b441c2f7ac89949e5e61a0dc7f3"
 
   depends_on "python@3.11"
+
+  livecheck do
+    url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/dataflow/releases/latest.json"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :json do |json, regex|
+      json["version"][regex, 1]
+    end
+  end
 
   def install
     system "#{Formula["python@3.11"].opt_bin}/python3.11", "-m", "pip", "install", "dataflow"
