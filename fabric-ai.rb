fdb0f87c4067c163da82277dd3508c9e9f7fd72e
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2025-01-13 08:36:26 -0300

    added fabric-ai

---
+++ fabric-ai.rb
@@ -0,0 +1,54 @@
+class FabricAi < Formula
+  desc "Fabric is an open-source framework for augmenting humans using AI"
+  homepage "https://github.com/danielmiessler/fabric"
+  version "1.4.130"
+  license "MIT"
+
+  livecheck do
+    url :homepage
+    regex(/v?(\d+(?:\.\d+)+[a-z]?)/i)
+    strategy :github_latest
+  end
+
+  if OS.linux?
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/danielmiessler/fabric/releases/download/v1.4.130/fabric-linux-arm64"
+      sha256 "400689333d3cf00fc3565353257bc2d53dd7e794f630b96a5f602e1eaf688f41"
+    end
+    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+      url "https://github.com/danielmiessler/fabric/releases/download/v1.4.130/fabric-linux-amd64"
+      sha256 "841fc30cffa83b0c8ed6bb1ee2973f0852cad9fa6d03033aee92f74fa929ac84"
+    end
+  end
+
+  if OS.mac?
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/danielmiessler/fabric/releases/download/v1.4.130/fabric-darwin-arm64"
+      sha256 "cd5522ef8b6b05162a2c72c92e4c11ae9dd5a7e1ceb6079dd53ec5738a3e1aec"
+    end
+    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+      url "https://github.com/danielmiessler/fabric/releases/download/v1.4.130/fabric-darwin-amd64"
+      sha256 "1bcae608909dbf8f193b237c72a81c757cc056cf4cc1da63ef08fd25cb600a64"
+    end
+  end
+
+  def install
+    if OS.linux?
+      if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+        bin.install "fabric-linux-arm64" => "fabric"
+      end
+      if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+        bin.install "fabric-linux-amd64" => "fabric"
+      end
+    end
+  
+    if OS.mac?
+      if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+        bin.install "fabric-darwin-arm64" => "fabric"
+      end
+      if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+        bin.install "fabric-darwin-amd64" => "fabric"
+      end
+    end
+  end
+end
