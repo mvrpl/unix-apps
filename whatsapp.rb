73b67759ec1493999bcb812163a7fab0e93b6d75
Marcos Lima <mvrpl.me@gmail.com>
2025-02-02 10:49:29 -0300

    update hashes

--- whatsapp.rb
+++ whatsapp.rb
@@ -3,42 +3,41 @@ class Whatsapp < Formula
     homepage "https://github.com/aldinokemal/go-whatsapp-web-multidevice"
     version "5.0.0"
     license "MIT"
-  
+
     livecheck do
-      url :homepage
-      regex(/v?(\d+(?:\.\d+)+[a-z]?)/i)
-      strategy :github_latest
+        url :homepage
+        regex(/v?(\d+(?:\.\d+)+[a-z]?)/i)
+        strategy :github_latest
     end
-  
+
     if OS.linux?
-      if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-        url "https://github.com/aldinokemal/go-whatsapp-web-multidevice/releases/download/v5.0.0/linux-arm64"
-        sha256 "a18d7b16282535b94664811ffb0e5445e1b562e3335695c0683af9169f916253"
-      end
-      if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-        url "https://github.com/aldinokemal/go-whatsapp-web-multidevice/releases/download/v5.0.0/linux-amd64"
-        sha256 "2e01657ae9bac498cf7564dbed7b8f6fcb55f897be66d86a972fc56e0173b117"
-      end
-    end
-  
-    if OS.mac? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-        url "https://github.com/aldinokemal/go-whatsapp-web-multidevice/releases/download/v5.0.0/darwin-amd64"
-        sha256 "edc77823b3779285a349be830bb7cd051ca79df40105c41eab868e62b13c4526"
-    end
-  
-    def install
-      if OS.linux?
         if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-          bin.install "linux-arm64" => "wapp"
+            url "https://github.com/aldinokemal/go-whatsapp-web-multidevice/releases/download/v5.0.0/linux-arm64"
+            sha256 "86464b34d1e8c63de9a2a4d62bc823f167e7030afdc1546c0d5b6420bfe3b8bd"
         end
         if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-          bin.install "linux-amd64" => "wapp"
+            url "https://github.com/aldinokemal/go-whatsapp-web-multidevice/releases/download/v5.0.0/linux-amd64"
+            sha256 "a7a4a8a60c6d761e89bd36c48c888c8710701f3148bde5343bd93e725de66c5b"
         end
-      end
-    
-      if OS.mac? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-        bin.install "darwin-amd64" => "wapp"
-      end
     end
-  end
-  
+
+    if OS.mac? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+        url "https://github.com/aldinokemal/go-whatsapp-web-multidevice/releases/download/v5.0.0/darwin-amd64"
+        sha256 "8bbc406cfc5e814d8c49d69acf524c9f12158b4d1f73b2d36a82dfaddbead918"
+    end
+
+    def install
+        if OS.linux?
+            if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+                bin.install "linux-arm64" => "wapp"
+            end
+            if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+                bin.install "linux-amd64" => "wapp"
+            end
+        end
+
+        if OS.mac? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+            bin.install "darwin-amd64" => "wapp"
+        end
+    end
+end
