6bf0464d16d9089cdb473c459fa9bbe63546bb51
mvrpl <7534519+mvrpl@users.noreply.github.com>
2025-03-19 00:18:29 +0000

    Apply automatic changes

--- Formula/otelcol-contrib.rb
+++ Formula/otelcol-contrib.rb
@@ -1,7 +1,7 @@
 class OtelcolContrib < Formula
   desc "OpenTelemetry Collector Contrib"
   homepage "https://github.com/open-telemetry/opentelemetry-collector-releases"
-  version "0.121.0"
+  version "0.122.0"
   license "Apache-2.0"
 
   livecheck do
@@ -11,18 +11,18 @@ class OtelcolContrib < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.121.0/otelcol-contrib_0.121.0_linux_amd64.tar.gz"
-    sha256 "f92e7945b5d2ca160b80f2fd2760a3bfc0db065945bafb5e0f5f80c3a6eebf22"
+    url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.122.0/otelcol-contrib_0.122.0_linux_amd64.tar.gz"
+    sha256 "22a053fc960ca5e1b936a3e8b3060322ff0bcf04ae14ec46d77f40c8d495a322"
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.121.0/otelcol-contrib_0.121.0_darwin_arm64.tar.gz"
-      sha256 "bf0eba4b80062d5f3dca90a54a4ccfcaabfddb2d76dfce9b78aa22e19aca045f"
+      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.122.0/otelcol-contrib_0.122.0_darwin_arm64.tar.gz"
+      sha256 "9ed9955153d8fc8c717b2e9bd07bd5e205c24298c2990f2534ad040446070933"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.121.0/otelcol-contrib_0.121.0_darwin_amd64.tar.gz"
-      sha256 "ef8f9e94cf8adc45409f0cb88847206d2d3be8f27cf0cb0839749b6daa9a4b79"
+      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.122.0/otelcol-contrib_0.122.0_darwin_amd64.tar.gz"
+      sha256 "2e83588ca2977fd02e3e4009e1a6ce9d08b8b56f75834ac04e7a998383bc63ec"
     end
   end
 
