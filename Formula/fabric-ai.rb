7cf8971901bbd270080cc492dac2d5f4d7c2c2a8
mvrpl <7534519+mvrpl@users.noreply.github.com>
2025-03-23 00:20:09 +0000

    Apply automatic changes

--- Formula/fabric-ai.rb
+++ Formula/fabric-ai.rb
@@ -1,7 +1,7 @@
 class FabricAi < Formula
   desc "Fabric is an open-source framework for augmenting humans using AI"
   homepage "https://github.com/danielmiessler/fabric"
-  version "1.4.163"
+  version "1.4.164"
   license "MIT"
 
   livecheck do
@@ -12,23 +12,23 @@ class FabricAi < Formula
 
   if OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/danielmiessler/fabric/releases/download/v1.4.163/fabric-linux-arm64"
-      sha256 "d1a02dedc202f78fd7370facae8bd756857a8b9070b5f120083e8f543d943ee3"
+      url "https://github.com/danielmiessler/fabric/releases/download/v1.4.164/fabric-linux-arm64"
+      sha256 "ba061cc082c35f33533f2de9f85ab7da07d5a2ce0e16373b8fdc532f57ca99f4"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/danielmiessler/fabric/releases/download/v1.4.163/fabric-linux-amd64"
-      sha256 "ff34a60820e27ce9d876df55367b9b1a61c838c42e0c8840c3381d54f6525a75"
+      url "https://github.com/danielmiessler/fabric/releases/download/v1.4.164/fabric-linux-amd64"
+      sha256 "360385f5d944bdd847dea1e1f3684194ed74131e7ac634d60bf7bc0b2d6aa38a"
     end
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/danielmiessler/fabric/releases/download/v1.4.163/fabric-darwin-arm64"
-      sha256 "40203474f2ec0648532c2c73281699d0142b0199427066b27c3602288e874cf3"
+      url "https://github.com/danielmiessler/fabric/releases/download/v1.4.164/fabric-darwin-arm64"
+      sha256 "b22c7f7089ee11e341260d149953372386a2bdf59e40f3476e65336d26b3a697"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/danielmiessler/fabric/releases/download/v1.4.163/fabric-darwin-amd64"
-      sha256 "d1a02dedc202f78fd7370facae8bd756857a8b9070b5f120083e8f543d943ee3"
+      url "https://github.com/danielmiessler/fabric/releases/download/v1.4.164/fabric-darwin-amd64"
+      sha256 "ba061cc082c35f33533f2de9f85ab7da07d5a2ce0e16373b8fdc532f57ca99f4"
     end
   end
 
