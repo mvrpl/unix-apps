1e6db2f8896c082a96bc9b44f9ef04a3a8efaa29
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2025-03-30 22:27:45 -0300

    adjusts cask

--- Casks/dockit.rb
+++ Casks/dockit.rb
@@ -1,19 +1,19 @@
 cask 'dockit' do
-    version '0.6.3'
-    sha256 '7faa0f72abff15592a8a233378b80100ea55153b06f0ff1dbf3b8e3a4ac63a00'
+    version "0.6.3"
+    sha256 "7faa0f72abff15592a8a233378b80100ea55153b06f0ff1dbf3b8e3a4ac63a00"
 
     url "https://github.com/geek-fun/dockit/releases/download/v0.6.3/DocKit_0.6.3_universal.dmg"
-    name 'DocKit'
-    homepage 'https://dockit.geekfun.club'
+    name "DocKit"
+    homepage "https://github.com/geek-fun/dockit"
     desc "NoSQL/NewSQL GUI client"
 
     auto_updates false
-    depends_on macos: '>= :monterey'
+    depends_on macos: ">= :monterey"
 
     app "DocKit.app"
 
     livecheck do
-        url "https://github.com/geek-fun/dockit"
+        url :homepage
         regex(/v?(\d+(?:\.\d+)+[a-z]?)/i)
         strategy :github_latest
     end
