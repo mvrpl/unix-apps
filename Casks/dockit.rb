2a6d199917197d0da706b7053e48e6851d82e217
Marcos Lima <mvrpl.me@gmail.com>
2025-04-01 06:21:06 -0300

    Update dockit.rb

--- Casks/dockit.rb
+++ Casks/dockit.rb
@@ -4,7 +4,7 @@ cask 'dockit' do
 
     url "https://github.com/geek-fun/dockit/releases/download/v0.6.4/DocKit_0.6.4_universal.dmg"
     name "DocKit"
-    homepage "https://github.com/geek-fun/dockit"
+    homepage "https://dockit.geekfun.club"
     desc "NoSQL/NewSQL GUI client"
 
     auto_updates false
@@ -13,7 +13,7 @@ cask 'dockit' do
     app "DocKit.app"
 
     livecheck do
-        url :homepage
+        url 'https://github.com/geek-fun/dockit'
         regex(/v?(\d+(?:\.\d+)+[a-z]?)/i)
         strategy :github_latest
     end
