4c013ee1061ff24e9cd69a8e73415da749a3705a
Marcos Lima <mvrpl.me@gmail.com>
2025-04-09 01:50:35 -0300

    Update desktop-chsht.rb

--- Casks/desktop-chsht.rb
+++ Casks/desktop-chsht.rb
@@ -1,6 +1,6 @@
 cask 'desktop-chsht' do
   version "0.2.1"
-  sha256 "267fc000141eb0f73433a17d8fca5710a02d32eed54198644d71703c809e57f7"
+  sha256 "f9c075c34d4e2edb55d4d5efc9b5714e9530761760b16a372595cf457a694c32"
 
   url "https://github.com/mvrpl/Desktop-Cheat-Sheet/releases/download/v0.2.1/Desktop-Cheat-Sheet.app.zip"
   name "Desktop Cheat Sheet"
