c50b43aa72377398704449d246f24ae5ab9fb850
mvrpl <7534519+mvrpl@users.noreply.github.com>
2025-04-21 04:04:59 +0000

    Apply automatic changes

--- Formula/chsht.rb
+++ Formula/chsht.rb
@@ -1,7 +1,7 @@
 class Chsht < Formula
   desc "Cheat Sheets on Terminal"
   homepage "https://github.com/mvrpl/Terminal-Cheat-Sheet"
-  version "1.0.0"
+  version "1.0.1"
   license "MIT"
 
   livecheck do
@@ -11,19 +11,19 @@ class Chsht < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/1.0.0/chsht-linux-amd64"
-    sha256 "9d07a29f6270d16b929bd23c7dfe747b7bc04be10dcab2bc5b984c08679d0469"
+    url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/1.0.1/chsht-linux-amd64"
+    sha256 "cb9386121baf399bb733cf195db9f59c4b27e53756edd35dc14e637b86d9b6b0"
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/1.0.0/chsht-macos-arm64"
-      sha256 "15b090211c28a76317b605564b2098ce6e75dcfe532f18a208dd7d76fe44092f"
+      url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/1.0.1/chsht-macos-arm64"
+      sha256 "5f383796cf4ff088060f2b8b756ff1eed2464ec660e26473f2dd1a2af991bc8b"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/1.0.0/chsht-macos-amd64"
-      sha256 "15b090211c28a76317b605564b2098ce6e75dcfe532f18a208dd7d76fe44092f"
+      url "https://artifacts-mvrpl.s3.sa-east-1.amazonaws.com/chsht/releases/1.0.1/chsht-macos-amd64"
+      sha256 "5f383796cf4ff088060f2b8b756ff1eed2464ec660e26473f2dd1a2af991bc8b"
     end
   end
 
