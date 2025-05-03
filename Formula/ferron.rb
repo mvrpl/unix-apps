fae56c820af7fa824b22cae594eca1e9983f53dc
mvrpl <7534519+mvrpl@users.noreply.github.com>
2025-05-03 12:10:41 +0000

    Apply automatic changes

--- Formula/ferron.rb
+++ Formula/ferron.rb
@@ -1,7 +1,7 @@
 class Ferron < Formula
   desc "A fast, memory-safe web server written in Rust"
   homepage "https://www.ferronweb.org"
-  version "1.1.2"
+  version "1.2.0"
   license "MIT"
 
   livecheck do
@@ -12,25 +12,25 @@ class Ferron < Formula
 
   if OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://downloads.ferronweb.org/1.1.2/ferron-1.1.2-aarch64-unknown-linux-gnu.zip"
-      sha256 "992e7b712e849c60ee4cf88e99cb2763569aa12eada99ef1f04256fe2cd6b936"
+      url "https://downloads.ferronweb.org/1.2.0/ferron-1.2.0-aarch64-unknown-linux-gnu.zip"
+      sha256 "cec568a5de3d272a0b5eb118b9d376a5dfc8292b251e4b21348fc0f4a81466cc"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://downloads.ferronweb.org/1.1.2/ferron-1.1.2-x86_64-unknown-linux-gnu.zip"
-      sha256 "cab4173babb3e98ca9e3a42a7a15d062842428c06b709c4ef627bc68646f53d6"
+      url "https://downloads.ferronweb.org/1.2.0/ferron-1.2.0-x86_64-unknown-linux-gnu.zip"
+      sha256 "186d5d3410d6a51023dc983a28407ac81f8699561e06a39e6ae9ac0f65caf69c"
     end
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://downloads.ferronweb.org/1.1.2/ferron-1.1.2-aarch64-apple-darwin.zip"
-      sha256 "334c712147373d68380446701e8e8cbed9ed5fc0fe1d9c523be5f9eb0095f2b5"
+      url "https://downloads.ferronweb.org/1.2.0/ferron-1.2.0-aarch64-apple-darwin.zip"
+      sha256 "c6b211ceb6b214e517eff76360ad7fa1bcb6ae40bb9468689cb00a456ad29353"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://downloads.ferronweb.org/1.1.2/ferron-1.1.2-x86_64-apple-darwin.zip"
-      sha256 "71ec1c1e15d9410aac56e42be7838857517bb0fdf2dcc41355c6d0fe253681e6"
+      url "https://downloads.ferronweb.org/1.2.0/ferron-1.2.0-x86_64-apple-darwin.zip"
+      sha256 "ef65994dc5c4591cd41103ce6ec46f4c35a42eeaf0409398a64fcffe2639cea1"
     end
   end
 
