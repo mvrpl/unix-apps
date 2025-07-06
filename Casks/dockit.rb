0666327557565d97ca78f2ee99f31d555aa3a03e
mvrpl <7534519+mvrpl@users.noreply.github.com>
2025-07-06 08:04:34 +0000

    Apply automatic changes

--- Casks/dockit.rb
+++ Casks/dockit.rb
@@ -1,8 +1,8 @@
 cask 'dockit' do
-    version "0.6.9"
-    sha256 "37fd8cded243466602f1372a0ecf23718edb475b43ad3ea72f252b8191aa9d27"
+    version "0.7.0"
+    sha256 "8c82e899e4a5e126e882628b606ce3d35c05ebb011f6970f02e198c93a7337a0"
 
-    url "https://github.com/geek-fun/dockit/releases/download/v0.6.9/DocKit_0.6.9_universal.dmg"
+    url "https://github.com/geek-fun/dockit/releases/download/v0.7.0/DocKit_0.7.0_universal.dmg"
     name "DocKit"
     homepage "https://dockit.geekfun.club"
     desc "NoSQL/NewSQL GUI client"
