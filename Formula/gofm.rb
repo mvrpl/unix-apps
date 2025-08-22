81687f3cff0422a3f17e3b0f325b97eb930fd4dd
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2025-08-22 19:32:42 -0300

    change github repo URL gofm

--- Formula/gofm.rb
+++ Formula/gofm.rb
@@ -1,6 +1,6 @@
 class Gofm < Formula
   desc "GoFM is a cross-platform real-time audio streaming server for MP3 files"
-  homepage "https://github.com/ssnat/GoFM"
+  homepage "https://github.com/pxgo/go-fm"
   version "0.2.4"
   license "MIT"
 
@@ -12,12 +12,12 @@ class Gofm < Formula
 
   if OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ssnat/GoFM/releases/download/v0.2.4/GoFM-linux-arm64-v0.2.4"
+      url "https://github.com/pxgo/go-fm/releases/download/v0.2.4/GoFM-linux-arm64-v0.2.4"
       sha256 "92a448c282b9c9b471373a522beca8cf79e451e8af05ae13cc0429d9f42ca3e5"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ssnat/GoFM/releases/download/v0.2.4/GoFM-linux-amd64-v0.2.4"
+      url "https://github.com/pxgo/go-fm/releases/download/v0.2.4/GoFM-linux-amd64-v0.2.4"
       sha256 "25f42d3250133c7deb07e8bf5ab943d352bc5c765f37793333b86ff2030176c4"
     end
   end
