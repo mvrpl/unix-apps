0f135e03a6b9929590188f92d32b7b4df307340a
Marcos Lima <mvrpl.me@gmail.com>
2025-09-19 21:19:57 -0300

    add tolk

---
+++ Formula/tolk.rb
@@ -0,0 +1,55 @@
+class Tolk < Formula
+  desc "Tolk is a next-generation language for developing smart contracts on TON blockchain"
+  homepage "https://github.com/ton-blockchain/ton"
+  version "1.1.0"
+  license "GPL-3.0"
+
+  livecheck do
+    url :homepage
+    regex(/tolk-([\d\.]+)/i)
+    strategy :github_latest
+  end
+
+  if OS.linux?
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/ton-blockchain/ton/releases/download/tolk-1.1.0/tolk-linux-arm64"
+      sha256 "10eae7d06793441e9a76ac372ba3a2bf4015baf69985a7b35c2a2ae1162ecd6a"
+    end
+
+    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+      url "https://github.com/ton-blockchain/ton/releases/download/tolk-1.1.0/tolk-linux-x86_64"
+      sha256 "2dd94a27049a0083ca6c268fccd6bcbb421ad6a8ee59d6a199dffdf81fdc3d7a"
+    end
+  end
+
+  if OS.mac?
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/ton-blockchain/ton/releases/download/tolk-1.1.0/tolk-mac-arm64"
+      sha256 "d7e817c4ae81e48275ebe954e4ef76f22d445e676d25a1664147b59e8866a526"
+    end
+
+    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+      url "https://github.com/ton-blockchain/ton/releases/download/tolk-1.1.0/tolk-mac-x86-64"
+      sha256 "8919c761021ae4b44840a5ae25559b9163bf90231f47b90000274c7f2032fbb2"
+    end
+  end
+
+  def install
+    if OS.linux?
+        if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+            bin.install "tolk-linux-arm64" => "tolk"
+        end
+        if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+            bin.install "tolk-linux-x86_64" => "tolk"
+        end
+    end
+    if OS.mac?
+        if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+            bin.install "tolk-mac-arm64" => "tolk"
+        end
+        if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+            bin.install "tolk-mac-x86-64" => "tolk"
+        end
+    end
+  end
+end
--- README.md
+++ README.md
@@ -36,6 +36,7 @@ brew tap mvrpl/unix-apps https://github.com/mvrpl/unix-apps
 - [![Brew Version](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fraw.githubusercontent.com%2Fmvrpl%2Funix-apps%2Frefs%2Fheads%2Fmain%2Fversions.json&query=%24.clio-server&style=flat&label=clio-server&color=%23701516)](https://github.com/mvrpl/unix-apps/blob/main/Formula/clio-server.rb) `brew install clio-server`
 - [![Brew Version](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fraw.githubusercontent.com%2Fmvrpl%2Funix-apps%2Frefs%2Fheads%2Fmain%2Fversions.json&query=%24.iota&style=flat&label=iota&color=%23701516)](https://github.com/mvrpl/unix-apps/blob/main/Formula/iota.rb) `brew install iota`
 - [![Brew Version](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fraw.githubusercontent.com%2Fmvrpl%2Funix-apps%2Frefs%2Fheads%2Fmain%2Fversions.json&query=%24.lassie&style=flat&label=lassie&color=%23701516)](https://github.com/mvrpl/unix-apps/blob/main/Formula/lassie.rb) `brew install lassie`
+- [![Brew Version](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fraw.githubusercontent.com%2Fmvrpl%2Funix-apps%2Frefs%2Fheads%2Fmain%2Fversions.json&query=%24.tolk&style=flat&label=tolk&color=%23701516)](https://github.com/mvrpl/unix-apps/blob/main/Formula/tolk.rb) `brew install tolk`
 
 ## GUI Applications (MacOS)
 
