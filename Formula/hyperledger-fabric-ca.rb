36a47483d0be6165a1ee2911d3957742701b64c6
Marcos Lima <mvrpl@icloud.com>
2025-10-25 17:42:24 -0300

    Update SHA256 checksums for hyperledger-fabric-ca

--- Formula/hyperledger-fabric-ca.rb
+++ Formula/hyperledger-fabric-ca.rb
@@ -13,24 +13,24 @@ class HyperledgerFabricCa < Formula
   if OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/hyperledger/fabric-ca/releases/download/v1.5.15/hyperledger-fabric-ca-linux-arm64-1.5.15.tar.gz"
-      sha256 "673deb2e05d97fe428d824e3ab1b3fadd04e97e3727d09cbbbfbbf6b686810e2"
+      sha256 "250f0666c54c419c29ebd4283310d475ccf7f778af36bb396c07b20bd416600a"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/hyperledger/fabric-ca/releases/download/v1.5.15/hyperledger-fabric-ca-linux-amd64-1.5.15.tar.gz"
-      sha256 "b823d48017fece94d1de5fa1d886041a629684fe86a7fa19525c56ef3002ab03"
+      sha256 "a4831c233ab7c1d9e039dc5badf4cf890719358442b5042f12d94b1d6f919289"
     end
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/hyperledger/fabric-ca/releases/download/v1.5.15/hyperledger-fabric-ca-darwin-arm64-1.5.15.tar.gz"
-      sha256 "921c0cee93606797abf9fc366a980e2f6346e79de0e3c4d4c68fd16abc513e01"
+      sha256 "e8c9edc85ee6e31d83f67c85fcde05410b86119286b5658d28a9d92a659d7c5b"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/hyperledger/fabric-ca/releases/download/v1.5.15/hyperledger-fabric-ca-darwin-amd64-1.5.15.tar.gz"
-      sha256 "a83586a29f6380f620dfcb7abeb338ed32c31c7cab9962b568dc767fa5fe0232"
+      sha256 "e90958e1613f45079e8c26a35ca6a6290f0460bafbd44c9cf65b738f5f59508d"
     end
   end
 
