62b80512757de8f51c5eebc4c435fbae923dd7c1
Marcos Lima <mvrpl.me@gmail.com>
2025-11-04 08:05:45 +0000

    Apply automatic changes

--- Formula/otelcol-contrib.rb
+++ Formula/otelcol-contrib.rb
@@ -1,7 +1,7 @@
 class OtelcolContrib < Formula
   desc "OpenTelemetry Collector Contrib"
   homepage "https://github.com/open-telemetry/opentelemetry-collector-releases"
-  version "0.138.0"
+  version "0.139.0"
   license "Apache-2.0"
 
   livecheck do
@@ -11,18 +11,18 @@ class OtelcolContrib < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.138.0/otelcol-contrib_0.138.0_linux_amd64.tar.gz"
-    sha256 "9f6462ffa45829bce9e69a2995932a521e9d40f0658eddbe27f6e262c2e82b1d"
+    url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.139.0/otelcol-contrib_0.139.0_linux_amd64.tar.gz"
+    sha256 "7d4356adf690b1a08f5d536331dc932ee28d31fd80801b6e960cd6b5618f86db"
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.138.0/otelcol-contrib_0.138.0_darwin_arm64.tar.gz"
-      sha256 "7aa1270c6c8914f87d7e93499f10fd360ef583f2164e0a49f270171b16bb274c"
+      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.139.0/otelcol-contrib_0.139.0_darwin_arm64.tar.gz"
+      sha256 "c85350e89a48ca5077585ce18b2060e134af398453954912f0f6d1638a5cce7e"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.138.0/otelcol-contrib_0.138.0_darwin_amd64.tar.gz"
-      sha256 "c0d2d7ca9cdab2c285fea6631881da7c6475976816d5d4b4c0714b8f3fbcb83c"
+      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.139.0/otelcol-contrib_0.139.0_darwin_amd64.tar.gz"
+      sha256 "93e124783840f4f480c7c996d482189ea0c71d78bf9d22e7fd1c8c74976d0615"
     end
   end
 
