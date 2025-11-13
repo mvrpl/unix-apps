2e884989699cee89a1cabfa5fe9e6789f6b232e0
Marcos Lima <mvrpl.me@gmail.com>
2025-11-13 06:51:52 -0300

    test

--- Formula/rippled.rb
+++ Formula/rippled.rb
@@ -22,7 +22,7 @@ class Rippled < Formula
 
   def install
     if OS.mac?
-      system "cd", "rippled-*"
+      system "cd", "rippled-2.6.1"
       system "conan", "config", "install", "conan/profiles/", "-tf", "$(conan config home)/profiles/"
       system "conan", "remote", "add", "--index", "0", "xrplf", "--force", "https://conan.ripplex.io"
       system "mkdir", ".build"
