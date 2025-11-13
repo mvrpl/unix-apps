28b3665c01339d0ea9f585900549d5706233c505
Marcos Lima <mvrpl.me@gmail.com>
2025-11-13 07:30:17 -0300

    test

--- Formula/rippled.rb
+++ Formula/rippled.rb
@@ -23,7 +23,7 @@ class Rippled < Formula
   def install
     if OS.mac?
       system "conan", "remote", "add", "--index", "0", "xrplf", "--force", "https://conan.ripplex.io"
-      system "conan", "install", ".", "--output-folder", ".", "--build", "missing", "--settings", "build_type=Release"
+      system "conan", "install", ".", "--output-folder", ".", "--build", "missing", "--settings", "build_type=Release", "--profile:build=default"
       system "cmake", "-DCMAKE_TOOLCHAIN_FILE:FILEPATH=build/generators/conan_toolchain.cmake", "-DCMAKE_BUILD_TYPE=Release", "-Dxrpld=ON", "-Dtests=ON", "."
       system "cmake", "--build", ".", "--config", "Release"
     end
