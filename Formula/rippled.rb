045d8c89393ebb4fe901d9b645926ad0b4ee3ff2
Marcos Lima <mvrpl.me@gmail.com>
2025-11-13 22:06:41 -0300

    test

--- Formula/rippled.rb
+++ Formula/rippled.rb
@@ -14,6 +14,7 @@ class Rippled < Formula
   depends_on "conan" => :build
   depends_on "cmake" => :build
   depends_on "gcc" => :build
+  depends_on "grpc"
 
   if OS.mac?
     url "https://github.com/XRPLF/rippled/archive/refs/tags/2.6.1.tar.gz"
@@ -23,13 +24,21 @@ class Rippled < Formula
   def install
     if OS.mac?
       Dir.mkdir(".build")
-      Dir.chdir(".build") do
-        system "conan", "profile", "detect", "--force"
-        system "conan", "remote", "add", "--index", "0", "xrplf", "--force", "https://conan.ripplex.io"
-        system "conan", "install", "..", "--output-folder", ".", "--build", "missing", "--settings", "build_type=Release"
-        system "cmake", "-DCMAKE_TOOLCHAIN_FILE:FILEPATH=build/generators/conan_toolchain.cmake", "-DCMAKE_BUILD_TYPE=Release", "-Dxrpld=ON", "-Dtests=ON", ".."
-        system "cmake", "--build", ".", "--config", "Release"
-      end
+      Dir.chdir(".build")
+      grpc = Formula["grpc"]
+      args = %W[
+        -DWITH_GRPC_INCLUDEDIR=#{grpc.opt_include}
+        -DWITH_GRPC_LIBDIR=#{grpc.opt_lib}
+        -DCMAKE_INSTALL_BINDIR=bin
+        -DCMAKE_INSTALL_LIBDIR=lib
+        -DCMAKE_INSTALL_INCLUDEDIR=include
+      ]
+      system "conan", "profile", "detect", "--force"
+      system "conan", "remote", "add", "--index", "0", "xrplf", "--force", "https://conan.ripplex.io"
+      system "conan", "install", "..", "--output-folder", ".", "--build", "missing", "--settings", "build_type=Release"
+      system "cmake", "-DCMAKE_TOOLCHAIN_FILE:FILEPATH=build/generators/conan_toolchain.cmake", "-DCMAKE_BUILD_TYPE=Release", "-Dxrpld=ON", "-Dtests=ON", ".."
+      system "cmake", "--build", ".", "--config", "Release", *args, *std_cmake_args
+      Dir.chdir("..")
     end
 
     bin.install "./.build/Release/rippled" => "rippled"
