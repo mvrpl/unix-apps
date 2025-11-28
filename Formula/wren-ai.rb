9e25f57ec3f1c620408f0e04a06043905409f47e
Marcos Lima <mvrpl.me@gmail.com>
2025-11-28 12:06:25 +0000

    Apply automatic changes

--- Formula/wren-ai.rb
+++ Formula/wren-ai.rb
@@ -1,7 +1,7 @@
 class WrenAi < Formula
   desc "Open-source GenBI AI Agent that empowers data-driven teams to chat with their data to generate Text-to-SQL, charts, spreadsheets, reports, and BI"
   homepage "https://github.com/Canner/WrenAI"
-  version "0.29.0"
+  version "0.29.1"
   license "AGPL-3.0"
 
   livecheck do
@@ -11,13 +11,13 @@ class WrenAi < Formula
   end
 
   if OS.linux? && Hardware::CPU.is_64_bit?
-    url "https://github.com/Canner/WrenAI/releases/download/0.29.0/wren-launcher-linux.tar.gz"
-    sha256 "37fb6877b0744d5f209fea41d5271df9c0b21172e95f95373f2017d22a359b5b"
+    url "https://github.com/Canner/WrenAI/releases/download/0.29.1/wren-launcher-linux.tar.gz"
+    sha256 "3dd8ff829387644b21452f3ce3438236a7d5e608b46c8be20dbc4b93b2aa8916"
   end
 
   if OS.mac? && Hardware::CPU.is_64_bit?
-    url "https://github.com/Canner/WrenAI/releases/download/0.29.0/wren-launcher-darwin.tar.gz"
-    sha256 "5a2f90bebbfd1857a4ed88b23dc8fb56e73adfe3b42307da404e7001b52129e0"
+    url "https://github.com/Canner/WrenAI/releases/download/0.29.1/wren-launcher-darwin.tar.gz"
+    sha256 "cac558a3af056920aaa2cf7cafd3ed1840d562994a3fe46879251df7f39012e8"
   end
 
   def install
