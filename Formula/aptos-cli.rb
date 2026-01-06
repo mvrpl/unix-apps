31bf997ff2e6c25f3bcee92687f8287bee219bbe
Marcos Lima <mvrpl.me@gmail.com>
2026-01-06 20:04:46 +0000

    Apply automatic changes

--- Formula/aptos-cli.rb
+++ Formula/aptos-cli.rb
@@ -1,7 +1,7 @@
 class AptosCli < Formula
   desc "Aptos is a layer 1 blockchain built to support the widespread use of blockchain through better technology and user experience"
   homepage "https://aptosfoundation.org"
-  version "7.12.1"
+  version "7.13.0"
   license "Apache-2.0"
 
   livecheck do
@@ -19,23 +19,23 @@ class AptosCli < Formula
 
   if OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v7.12.1/aptos-cli-7.12.1-Linux-aarch64.zip"
-      sha256 "2f9d594f1b68d12a6ee3ee78d44f4233554d840a8e5e7f4029b227fbfe0c712f"
+      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v7.13.0/aptos-cli-7.13.0-Linux-aarch64.zip"
+      sha256 "10a8316d9ae287af8c046523cd8b4699072fa47c860a095cf22e394f2e65b0f5"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v7.12.1/aptos-cli-7.12.1-Linux-x86_64.zip"
-      sha256 "76d3f5524200ff1259dcaf3b4eeb053ac7e1c7af9fa95a2117149428786106d6"
+      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v7.13.0/aptos-cli-7.13.0-Linux-x86_64.zip"
+      sha256 "f2e364dfae17b20137eb57151fec4f5d06bd0f9fae740dd178fc2696b762851b"
     end
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v7.12.1/aptos-cli-7.12.1-macOS-arm64.zip"
-      sha256 "4a2c4c5ecef87b871a684f6b0c767ac4a6c2cae464766559f1d8be641b157133"
+      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v7.13.0/aptos-cli-7.13.0-macOS-arm64.zip"
+      sha256 "101a7c17bf0e341c25bd0e78f531326a096f25508e788f981a3fefc35077108f"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v7.12.1/aptos-cli-7.12.1-macOS-x86_64.zip"
-      sha256 "99c7165a16f1ddc6251557fc1f6626229ee16032f9d41eade02e3efc4c83cc47"
+      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v7.13.0/aptos-cli-7.13.0-macOS-x86_64.zip"
+      sha256 "70059172f8966bdf5e2feb516256b36cbdaf0e1bf513d1d11fc4edf2177adac4"
     end
   end
 
--- Formula/otelcol-contrib.rb
+++ Formula/otelcol-contrib.rb
@@ -1,7 +1,7 @@
 class OtelcolContrib < Formula
   desc "OpenTelemetry Collector Contrib"
   homepage "https://github.com/open-telemetry/opentelemetry-collector-releases"
-  version "0.143.0"
+  version "0.143.1"
   license "Apache-2.0"
 
   livecheck do
@@ -11,18 +11,18 @@ class OtelcolContrib < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.143.0/otelcol-contrib_0.143.0_linux_amd64.tar.gz"
-    sha256 "de3d8fd2a30d8df30a940df0500ae9af59ab807783b1529c776ba40eb0a05981"
+    url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.143.1/otelcol-contrib_0.143.1_linux_amd64.tar.gz"
+    sha256 "e6d26dde5e43d09fdde6289d8afcdf057687314a35813006c4e65b23a3f95356"
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.143.0/otelcol-contrib_0.143.0_darwin_arm64.tar.gz"
-      sha256 "ba6954a104c1a2e71307ef60d967703f71953adfa538bf41d6abf43c446bdd53"
+      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.143.1/otelcol-contrib_0.143.1_darwin_arm64.tar.gz"
+      sha256 "2afeaf0762cf9657525b0e6a060a38fb8f8e64ebc1787fccf8a5ad7e3803ff55"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.143.0/otelcol-contrib_0.143.0_darwin_amd64.tar.gz"
-      sha256 "eb0a983712df537cccc88333e1581dcd8eee28f0510053783b14bfd0bbf94b49"
+      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.143.1/otelcol-contrib_0.143.1_darwin_amd64.tar.gz"
+      sha256 "4a243e0060b084b9c190b1226d703bf8e960a68765d7590576e250fa1dbcc837"
     end
   end
 
