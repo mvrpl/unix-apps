a4596d3bd2eaa8fd39c12e465a4434f241014125
Marcos Lima <mvrpl.me@gmail.com>
2026-01-20 20:24:10 +0000

    Apply automatic changes

--- Formula/otelcol-contrib.rb
+++ Formula/otelcol-contrib.rb
@@ -1,7 +1,7 @@
 class OtelcolContrib < Formula
   desc "OpenTelemetry Collector Contrib"
   homepage "https://github.com/open-telemetry/opentelemetry-collector-releases"
-  version "0.143.1"
+  version "0.144.0"
   license "Apache-2.0"
 
   livecheck do
@@ -11,18 +11,18 @@ class OtelcolContrib < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.143.1/otelcol-contrib_0.143.1_linux_amd64.tar.gz"
-    sha256 "e6d26dde5e43d09fdde6289d8afcdf057687314a35813006c4e65b23a3f95356"
+    url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.144.0/otelcol-contrib_0.144.0_linux_amd64.tar.gz"
+    sha256 "4429eecfb311e35f846af453d46c0a8a72e6773489e8105fb049f5bc9edec022"
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.143.1/otelcol-contrib_0.143.1_darwin_arm64.tar.gz"
-      sha256 "2afeaf0762cf9657525b0e6a060a38fb8f8e64ebc1787fccf8a5ad7e3803ff55"
+      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.144.0/otelcol-contrib_0.144.0_darwin_arm64.tar.gz"
+      sha256 "605cdc88c65b23d7e0c373d56a1f411da427a978567ab186050a27791f7779b0"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.143.1/otelcol-contrib_0.143.1_darwin_amd64.tar.gz"
-      sha256 "4a243e0060b084b9c190b1226d703bf8e960a68765d7590576e250fa1dbcc837"
+      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.144.0/otelcol-contrib_0.144.0_darwin_amd64.tar.gz"
+      sha256 "0de9275b11bf6285648470fb8efec5612cb422f495df7a3fef93cd8cd647ed0e"
     end
   end
 
