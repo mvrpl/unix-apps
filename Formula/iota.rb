766656eb4b9c7b7b44da905151aca003376daec5
Marcos Lima <mvrpl.me@gmail.com>
2026-02-25 08:15:21 +0000

    Apply automatic changes

--- Formula/iota.rb
+++ Formula/iota.rb
@@ -1,7 +1,7 @@
 class Iota < Formula
   desc "Bringing the real world to Web3 with a scalable, decentralized and programmable DLT infrastructure"
   homepage "https://github.com/iotaledger/iota"
-  version "1.16.2"
+  version "1.17.2"
   license "Apache-2.0"
 
   livecheck do
@@ -22,19 +22,19 @@ class Iota < Formula
 
   if OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/iotaledger/iota/releases/download/v1.16.2/iota-v1.16.2-linux-arm64.tgz"
-      sha256 "3960264543a6d0979c65d030eb9504555664d9132cdac7afefe3e0d57a6b1347"
+      url "https://github.com/iotaledger/iota/releases/download/v1.17.2/iota-v1.17.2-linux-arm64.tgz"
+      sha256 "4e21c5ca2dcbb8a386193ab194fd99854cc9ddc84b772b06e2f1d481f98b84f2"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/iotaledger/iota/releases/download/v1.16.2/iota-v1.16.2-linux-x86_64.tgz"
-      sha256 "0f178f33d40a68df79f06eff7ac1f322607fd4fd5f4f01878c044c36db21fef0"
+      url "https://github.com/iotaledger/iota/releases/download/v1.17.2/iota-v1.17.2-linux-x86_64.tgz"
+      sha256 "d18f837029d96d9d05ae35a70e00dd9cf4f485d3ba887d83d584265b2c30c5ee"
     end
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/iotaledger/iota/releases/download/v1.16.2/iota-v1.16.2-macos-arm64.tgz"
-      sha256 "b65f1523e1da233ce1fce321c6d43a992674357c7478c9b2109cca69661017d6"
+      url "https://github.com/iotaledger/iota/releases/download/v1.17.2/iota-v1.17.2-macos-arm64.tgz"
+      sha256 "58d260d402a5fbb86ee9ae960959958ea0de2ef37cd12804395622f8298caea1"
     end
   end
 
