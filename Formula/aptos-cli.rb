826fc4414deadc02ab21f9ace681db76f3120306
Marcos Lima <mvrpl.me@gmail.com>
2026-04-21 20:18:49 +0000

    Apply automatic changes

--- Formula/aptos-cli.rb
+++ Formula/aptos-cli.rb
@@ -1,7 +1,7 @@
 class AptosCli < Formula
   desc "Aptos is a layer 1 blockchain built to support the widespread use of blockchain through better technology and user experience"
   homepage "https://aptosfoundation.org"
-  version "9.1.0"
+  version "9.2.0"
   license "Apache-2.0"
 
   livecheck do
@@ -19,23 +19,23 @@ class AptosCli < Formula
 
   if OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v9.1.0/aptos-cli-9.1.0-Linux-aarch64.zip"
-      sha256 "2fae0588e63e86853bf69d6cd38c604ccf26b1321af2929287e5140bab09cd42"
+      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v9.2.0/aptos-cli-9.2.0-Linux-aarch64.zip"
+      sha256 "f3d856e3023eb71b525135c375ccbe1714788c0c2ccafc3f2359dc877b147a39"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v9.1.0/aptos-cli-9.1.0-Linux-x86_64.zip"
-      sha256 "594a9086126d60ec5ee2d74a0ebab8b81a2f91488f9508bf48f22d91026ae2ea"
+      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v9.2.0/aptos-cli-9.2.0-Linux-x86_64.zip"
+      sha256 "c6c0ae20e8599c7085da9c0a4996115093dcffaa5631704f5ae29eed35fb2a75"
     end
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v9.1.0/aptos-cli-9.1.0-macOS-arm64.zip"
-      sha256 "49a2cb2eebff9044f4674102f3cbe5b6d2c436e2eecf12609485f98190c5588e"
+      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v9.2.0/aptos-cli-9.2.0-macOS-arm64.zip"
+      sha256 "695fc75d8cd3201680e2850cf6ee7cfdb53b51057892af2eb1e5ab1b6c8d436a"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v9.1.0/aptos-cli-9.1.0-macOS-x86_64.zip"
-      sha256 "277fc56f7a8251be6f84f4a2c90f2c19dc456f34d44042401e9bed5acfef2bcf"
+      url "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v9.2.0/aptos-cli-9.2.0-macOS-x86_64.zip"
+      sha256 "957c358f268d60c84002102ca5a125f392c45ca0df6b82559a3d9e5cd2bba007"
     end
   end
 
--- Formula/dotenvx.rb
+++ Formula/dotenvx.rb
@@ -1,7 +1,7 @@
 class Dotenvx < Formula
   desc "A secure dotenv from the creator of 'dotenv'"
   homepage "https://github.com/dotenvx/dotenvx"
-  version "1.61.1"
+  version "1.61.2"
   license "BSD-3-Clause"
 
   livecheck do
@@ -12,24 +12,24 @@ class Dotenvx < Formula
 
   if OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dotenvx/dotenvx/releases/download/v1.61.1/dotenvx-linux-arm64.tar.gz"
-      sha256 "04f0aa74a60a0d09e2bbc8fc4f532f2ffe164c37eb90a2059c32df0fe1f60ee9"
+      url "https://github.com/dotenvx/dotenvx/releases/download/v1.61.2/dotenvx-linux-arm64.tar.gz"
+      sha256 "1cd685bef52c5e924864ccfd45dfa17f7088c7ad1a6baf5256a760f09b65d8d8"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dotenvx/dotenvx/releases/download/v1.61.1/dotenvx-linux-x86_64.tar.gz"
-      sha256 "98c98b203701eb3af7682da4966f221221c58e47a8b6c60f2e9b00b607762a6f"
+      url "https://github.com/dotenvx/dotenvx/releases/download/v1.61.2/dotenvx-linux-x86_64.tar.gz"
+      sha256 "bd5a48c91a2e96271efb90e8e5012645b8b9f66239522efbf89d7e0fa9691e32"
     end
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dotenvx/dotenvx/releases/download/v1.61.1/dotenvx-darwin-arm64.tar.gz"
-      sha256 "1d3c609877742af6270e6a913640806fd000e0bc5b80ba0847248349ca9c4d95"
+      url "https://github.com/dotenvx/dotenvx/releases/download/v1.61.2/dotenvx-darwin-arm64.tar.gz"
+      sha256 "eed15b15222a30895d4f35df1da46914cb833ade26c4d5707c278fcc91e425a0"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dotenvx/dotenvx/releases/download/v1.61.1/dotenvx-darwin-x86_64.tar.gz"
-      sha256 "2c3f9920bb6ae8f910a23de695148e2079598d108833aa71fa08907f354b74c9"
+      url "https://github.com/dotenvx/dotenvx/releases/download/v1.61.2/dotenvx-darwin-x86_64.tar.gz"
+      sha256 "bb2a2cf9e67be5a6d050995b8944d7af944b0989d800dadbbfaeec70ffdb5499"
     end
   end
 
