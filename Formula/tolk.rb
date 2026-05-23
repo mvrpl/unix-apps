5b71cb6687d2a3f0c2484f38f4ccaff8b033b71d
Marcos Lima <mvrpl.me@gmail.com>
2026-05-23 05:15:55 +0000

    Apply automatic changes

--- Formula/tolk.rb
+++ Formula/tolk.rb
@@ -1,7 +1,7 @@
 class Tolk < Formula
   desc "Tolk is a next-generation language for developing smart contracts on TON blockchain"
   homepage "https://github.com/ton-blockchain/ton"
-  version "1.3.0"
+  version "1.4.1"
   license "GPL-3.0"
 
   livecheck do
@@ -14,25 +14,25 @@ class Tolk < Formula
 
   if OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ton-blockchain/ton/releases/download/tolk-1.3.0/tolk-linux-arm64"
-      sha256 "c4c9e52daa8b31b5d9dbad7ecea4801929b8a66dbf9797c3841b8e6e6b43039b"
+      url "https://github.com/ton-blockchain/ton/releases/download/tolk-1.4.1/tolk-linux-arm64"
+      sha256 "7cad325c7827bd4d1239f294051a53816998cb5b8de67d92c9e43a2a8abb7213"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ton-blockchain/ton/releases/download/tolk-1.3.0/tolk-linux-x86_64"
-      sha256 "4bf385302474e32b5f19f6f2482e05496910e29254a0e76544ec1fc4ba2e8bcc"
+      url "https://github.com/ton-blockchain/ton/releases/download/tolk-1.4.1/tolk-linux-x86_64"
+      sha256 "4696c724bcac452d260cae35445c580a3a5e01ccf00d92dff1a595492c76e315"
     end
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ton-blockchain/ton/releases/download/tolk-1.3.0/tolk-mac-arm64"
-      sha256 "1b5aca495cae6ea9388b5aee64521065e295224872003258836b3ab072aa507c"
+      url "https://github.com/ton-blockchain/ton/releases/download/tolk-1.4.1/tolk-mac-arm64"
+      sha256 "636503bb9d14d5cfe09fee8108bb6a5e179c5a5013404ba462ef16925ca43635"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ton-blockchain/ton/releases/download/tolk-1.3.0/tolk-mac-x86-64"
-      sha256 "4fcd4c2a83679e261bf18d245d5dc93a46348045500504dcd25d2f51cca6a397"
+      url "https://github.com/ton-blockchain/ton/releases/download/tolk-1.4.1/tolk-mac-x86-64"
+      sha256 "c7ce0f9c501d82b7e0ea417798535c92e34fef0f995fcce3229683dee02133c1"
     end
   end
 
