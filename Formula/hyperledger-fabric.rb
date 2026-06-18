6f8e6cd8b6bfa4097d1557ef24898539e9c33556
Marcos Lima <mvrpl.me@gmail.com>
2026-06-18 09:31:42 +0000

    Apply automatic changes

--- Formula/hyperledger-fabric.rb
+++ Formula/hyperledger-fabric.rb
@@ -1,7 +1,7 @@
 class HyperledgerFabric < Formula
   desc "Hyperledger Fabric is an enterprise-grade permissioned distributed ledger framework for developing solutions and applications"
   homepage "https://github.com/hyperledger/fabric"
-  version "3.1.4"
+  version "3.1.5"
   license "Apache-2.0"
 
   livecheck do
@@ -12,25 +12,25 @@ class HyperledgerFabric < Formula
 
   if OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/hyperledger/fabric/releases/download/v3.1.4/hyperledger-fabric-linux-arm64-3.1.4.tar.gz"
-      sha256 "10ef1f5440a33fc5d90b88f8d0417037beeb44d67be431e6f35516e6007cfc75"
+      url "https://github.com/hyperledger/fabric/releases/download/v3.1.5/hyperledger-fabric-linux-arm64-3.1.5.tar.gz"
+      sha256 "4a42cad740a7e0373eda6bf1e27c86e9b06132f9bb751daf7f0f7dc1d1bbfed9"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/hyperledger/fabric/releases/download/v3.1.4/hyperledger-fabric-linux-amd64-3.1.4.tar.gz"
-      sha256 "9c197263d3eba0f5a0b0f5ef14d64922131e5a0fd0f94b12b20fe9d1d96ed5ed"
+      url "https://github.com/hyperledger/fabric/releases/download/v3.1.5/hyperledger-fabric-linux-amd64-3.1.5.tar.gz"
+      sha256 "b9c31fd490991e76f8acb1835dee09fc19fee5428cb13e190ee6e0bdd2c37858"
     end
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/hyperledger/fabric/releases/download/v3.1.4/hyperledger-fabric-darwin-arm64-3.1.4.tar.gz"
-      sha256 "b7d92385c871daa405a78d8abd9edd1b7061fdf9e5246dad9b9048c1681ee6a2"
+      url "https://github.com/hyperledger/fabric/releases/download/v3.1.5/hyperledger-fabric-darwin-arm64-3.1.5.tar.gz"
+      sha256 "d67a5c3366746a984f8ee9ae0ebf5c8b357c9d33dd589fa0b7668d739472cef0"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/hyperledger/fabric/releases/download/v3.1.4/hyperledger-fabric-darwin-amd64-3.1.4.tar.gz"
-      sha256 "54233afb606a500c6a5efadfce90dafdf7c789e8d6fbd48d4477210ab9864d65"
+      url "https://github.com/hyperledger/fabric/releases/download/v3.1.5/hyperledger-fabric-darwin-amd64-3.1.5.tar.gz"
+      sha256 "866b64b036ba9eff51ba2131bbe34b4b9057192f5766859ede22ea1cdd564158"
     end
   end
 
