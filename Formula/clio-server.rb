0639830e61a4449983eaa2b0672fdf67843a488b
Marcos Lima <mvrpl.me@gmail.com>
2026-07-20 13:54:29 +0000

    Apply automatic changes

--- Formula/clio-server.rb
+++ Formula/clio-server.rb
@@ -1,7 +1,7 @@
 class ClioServer < Formula
   desc "An XRP Ledger API Server"
   homepage "https://github.com/XRPLF/clio"
-  version "2.7.0"
+  version "2.7.1"
   license "ISC"
 
   livecheck do
@@ -11,13 +11,13 @@ class ClioServer < Formula
   end
 
   if OS.linux?
-    url "https://github.com/XRPLF/clio/releases/download/2.7.0/clio_server_Linux_Release_gcc.zip"
-    sha256 "f5bae504b6b002f8ea04ea01325c87c9520507c71226c03cfca7e8f8ee694ea3"
+    url "https://github.com/XRPLF/clio/releases/download/2.7.1/clio_server_Linux_Release_gcc.zip"
+    sha256 "e74eb44d9e9d232753e5733bbf97ed68e28bf458682add9920acf9028fd6d5c4"
   end
 
   if OS.mac?
-    url "https://github.com/XRPLF/clio/releases/download/2.7.0/clio_server_macOS_Release_apple-clang.zip"
-    sha256 "7649ad0bd8af7be4b980dbc4c7b5901fc3d4af63f62c744e675da4a6f334b4aa"
+    url "https://github.com/XRPLF/clio/releases/download/2.7.1/clio_server_macOS_Release_apple-clang.zip"
+    sha256 "71f2c3177f258cf72d2b6630f925617960944bcf9b760177ba3ede7f18c561e0"
   end
 
   def install
