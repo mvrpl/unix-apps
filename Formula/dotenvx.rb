7ca0c8def9264ee79c08f4c64859ba424d0610ec
Marcos Lima <mvrpl.me@gmail.com>
2026-07-27 06:58:33 +0000

    Apply automatic changes

--- Formula/dotenvx.rb
+++ Formula/dotenvx.rb
@@ -1,7 +1,7 @@
 class Dotenvx < Formula
   desc "A secure dotenv from the creator of 'dotenv'"
   homepage "https://github.com/dotenvx/dotenvx"
-  version "2.18.1"
+  version "2.19.0"
   license "BSD-3-Clause"
 
   livecheck do
@@ -12,24 +12,24 @@ class Dotenvx < Formula
 
   if OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dotenvx/dotenvx/releases/download/v2.18.1/dotenvx-linux-arm64.tar.gz"
-      sha256 "ac4790e4dafeff098e3a8afdf03ff894ced60402e802ab741dd6c289ba023413"
+      url "https://github.com/dotenvx/dotenvx/releases/download/v2.19.0/dotenvx-linux-arm64.tar.gz"
+      sha256 "49bf227eccd55ae1264a468fd7c92e3157b5fb1f93dbf7fe34bf849662d9fdec"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dotenvx/dotenvx/releases/download/v2.18.1/dotenvx-linux-x86_64.tar.gz"
-      sha256 "eca89fcd68db79afaf18c85bcb9982103cd1bf88f28278cf9a5f42a037c601ba"
+      url "https://github.com/dotenvx/dotenvx/releases/download/v2.19.0/dotenvx-linux-x86_64.tar.gz"
+      sha256 "fce261384c96716f836f88e89e39ba694724f5d7a836f0d246556db6c1da62e7"
     end
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dotenvx/dotenvx/releases/download/v2.18.1/dotenvx-darwin-arm64.tar.gz"
-      sha256 "6d3aa41e5e985c467a67c22f8f34c3116140862d924434da36984379ae103795"
+      url "https://github.com/dotenvx/dotenvx/releases/download/v2.19.0/dotenvx-darwin-arm64.tar.gz"
+      sha256 "2ff1b6e09cc3f9761ee15eff140475c057f77bf1ebd762ae7cf5050b6f8a39b7"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dotenvx/dotenvx/releases/download/v2.18.1/dotenvx-darwin-x86_64.tar.gz"
-      sha256 "2aab163d2dda08d5fa85e0ceb922e9398542a996f518116a28e0fb7472186cec"
+      url "https://github.com/dotenvx/dotenvx/releases/download/v2.19.0/dotenvx-darwin-x86_64.tar.gz"
+      sha256 "47271559fb16e9d8d29d4075e8a72fd5266739a0f2c8d134dc876097a7f4aeaa"
     end
   end
 
