d84d1dc9516088b9ad537c2e9f37ae8e5e44a0f3
Marcos Lima <mvrpl.me@gmail.com>
2026-07-30 10:19:33 +0000

    Apply automatic changes

--- Casks/dockit.rb
+++ Casks/dockit.rb
@@ -1,8 +1,8 @@
 cask 'dockit' do
-    version "1.3.0"
-    sha256 "50b18ece1d86c08671df7308b566d61441baa2a891b1cd63852139765a8354a7"
+    version "1.3.1"
+    sha256 "19fbc4b1f0214f9bc9b882d5a643fc0f359ef14c25949dbfe3690271c6c3e868"
 
-    url "https://github.com/geek-fun/dockit/releases/download/v1.3.0/DocKit_1.3.0_universal.dmg"
+    url "https://github.com/geek-fun/dockit/releases/download/v1.3.1/DocKit_1.3.1_universal.dmg"
     name "DocKit"
     homepage "https://dockit.geekfun.club"
     desc "NoSQL/NewSQL GUI client"
