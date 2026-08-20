4516d229dd7b1872d09125124776f7ef7ae3ddd4
Marcos Lima <mvrpl.me@gmail.com>
2026-08-20 20:21:49 +0000

    Apply automatic changes

--- Formula/screego.rb
+++ Formula/screego.rb
@@ -1,7 +1,7 @@
 class Screego < Formula
   desc "Screen sharing for developers"
   homepage "https://screego.net"
-  version "1.12.4"
+  version "1.12.5"
   license "GPL-3.0"
 
   livecheck do
@@ -12,25 +12,25 @@ class Screego < Formula
 
   if OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/screego/server/releases/download/v1.12.4/screego_1.12.4_linux_arm64.tar.gz"
-      sha256 "47026d78cd0a8bd1f039a899a0b4015b4e0ead2b513e9caa6cd2e3c11f135810"
+      url "https://github.com/screego/server/releases/download/v1.12.5/screego_1.12.5_linux_arm64.tar.gz"
+      sha256 "e47383e6c9adb96b4226772cd59f78d823a320b4553d0a79b262e54bd724980e"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/screego/server/releases/download/v1.12.4/screego_1.12.4_linux_amd64.tar.gz"
-      sha256 "f7dc5ee1f32cec7691cb5c51d9dd9b618cfdda25e7fe59360b6014e308cbb652"
+      url "https://github.com/screego/server/releases/download/v1.12.5/screego_1.12.5_linux_amd64.tar.gz"
+      sha256 "60e69336ec69c0091a988b7f6fb299aeae752b10f24abad4c34d35282ac4fb4a"
     end
   end
 
   if OS.mac?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/screego/server/releases/download/v1.12.4/screego_1.12.4_darwin_arm64.tar.gz"
-      sha256 "2ecfdcd3e1f41ffc51ffd15eb2c30563be0ac3c39526846306b19e7388a17cc0"
+      url "https://github.com/screego/server/releases/download/v1.12.5/screego_1.12.5_darwin_arm64.tar.gz"
+      sha256 "1c65908d620ce5d5f0efd0bd4aad28062853c50a9292fb52d5f2ca4a5f486800"
     end
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/screego/server/releases/download/v1.12.4/screego_1.12.4_darwin_amd64.tar.gz"
-      sha256 "711658a1bdc4db9c84b1123c3811f8295dd56f7a5c8f4870c42c9db672f082a2"
+      url "https://github.com/screego/server/releases/download/v1.12.5/screego_1.12.5_darwin_amd64.tar.gz"
+      sha256 "8d48cda160acdb7dbc48b93277595c2e24d7ce419d3d0d73bf205d7cb782eb39"
     end
   end
 
